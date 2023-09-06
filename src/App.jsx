3f08676bf7e00601f25ccb3fb056e051236c204d
Andrey <kedr_club@mail.ru>
2023-09-06 11:26:11 +0300

    feat: add triangle component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ import Triangle from './components/Triangle';
 // import Cone from './components/Cone';
 
 export default function App() {
+  const H = 3;
+  const positions1 = new Float32Array([-2, -H, 0, 0, H, 0, -4, -H + 1, 0]);
+  const positions2 = new Float32Array([-4, -H + 1, 0, 0, H, 0, -6, 0, 0]);
+
+  const props1 = { H: 3, position: positions1 };
+  const props2 = { H: 3, position: positions2 };
+
+  const positions = [positions1, positions2];
+
+  // let cone = { height: 3, radius: 2 };
+
   return (
     <div className="canvas-container">
       <Canvas>
@@ -15,9 +26,13 @@ export default function App() {
         <OrbitControls />
         <Stars />
         <directionalLight color="white" position={[0, 0, 5]} />
-        {/* <Box />
-        <Cone /> */}
-        <Triangle />
+        {/* <Box /> */}
+        {/* <Cone /> */}
+        {positions.map((el, index) => {
+          return <Triangle key={index} props={el} />;
+        })}
+        <Triangle props={props1} />
+        <Triangle props={props2} />
       </Canvas>
     </div>
   );
--- src/components/Triangle.jsx
+++ src/components/Triangle.jsx
@@ -1,10 +1,39 @@
 /* eslint-disable react/no-unknown-property */
+import { DoubleSide } from 'three';
 
-function Triangle() {
+function Triangle(cone) {
+  const positions = cone.props.position;
+
+  const normals = new Float32Array([0, 0, 1, 0, 0, 1, 0, 0, 1]);
+
+  const colors = new Float32Array([0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1]);
+
+  //   const indixes = new Uint16Array([0, 1, 3, 2, 3]);
+
   return (
     <mesh>
-      <triangleGeometry />
-      <meshStandardMaterial />
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+        <bufferAttribute
+          attach="attributes-color"
+          array={colors}
+          count={colors.length / 3}
+          itemSize={3}
+        />
+        <bufferAttribute
+          attach="attributes-normal"
+          array={normals}
+          count={normals.length / 3}
+          itemSize={3}
+        />
+        {/* <bufferAttribute attach="index" array={indixes} count={indixes.length} itemSize={1} /> */}
+      </bufferGeometry>
+      <meshStandardMaterial vertexColors side={DoubleSide} />
     </mesh>
   );
 }
